89955bf5f3897ee91846074d44239e29b55191c6
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-07-31 18:14:35 +0300

    Update base_widget.dart

---
+++ lib/core/base/view/base_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class BaseView<T> extends StatefulWidget {
+  final Widget Function(BuildContext context, T value) onPageBuilder;
+  final T? viewModel;
+  final Function(T model)? onModelReady;
+  final VoidCallback? onDispose;
+
+  const BaseView(
+      {Key? key,
+      this.onDispose,
+      this.onModelReady,
+      this.viewModel,
+      required this.onPageBuilder})
+      : super(key: key);
+
+  @override
+  State<BaseView> createState() => _BaseViewState();
+}
+
+class _BaseViewState extends State<BaseView> {
+  @override
+  void initState() {
+    super.initState();
+    widget.onModelReady?.call(widget.viewModel);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    widget.onDispose?.call();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.onPageBuilder(context, widget.viewModel);
+  }
+}
