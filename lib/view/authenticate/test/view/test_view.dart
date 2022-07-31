378d558b135801c2c4363b6bc1d41d8adf6ca8e7
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-07-31 17:45:41 +0300

    Initial commit

---
+++ lib/view/authenticate/test/view/test_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_template/core/base/state/base_state.dart';
+
+class TestsView extends StatefulWidget {
+  const TestsView({Key? key}) : super(key: key);
+
+  @override
+  BaseState<TestsView> createState() => _TestsViewState();
+}
+
+class _TestsViewState extends BaseState<TestsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: dynamicHeight(0.1),
+      color: themeData.primaryColor,
+      child: Placeholder(
+
+      ),
+
+    );
+  }
+}
---
+++ test/feature/login/login_test.dart
@@ -0,0 +1,12 @@
+main(){
+setup(){
+print("test");
+}
+
+test("User Login Fail Test", (){
+final isUserLogin=true;
+
+expect(isUserLogin, isTrue);
+
+});
+}
