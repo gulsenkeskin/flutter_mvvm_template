0cc98c03c65a2a51bdbddf71ed60b862f337107f
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-13 00:08:54 +0300

    LanguageManager added

--- lib/view/authenticate/test/viewmodel/test_view_model.g.dart
+++ lib/view/authenticate/test/viewmodel/test_view_model.g.dart
@@ -9,6 +9,13 @@ part of 'test_view_model.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$TestViewModel on _TestViewModelBase, Store {
+  Computed<bool>? _$isEvenComputed;
+
+  @override
+  bool get isEven => (_$isEvenComputed ??=
+          Computed<bool>(() => super.isEven, name: '_TestViewModelBase.isEven'))
+      .value;
+
   late final _$numberAtom =
       Atom(name: '_TestViewModelBase.number', context: context);
 
@@ -42,7 +49,8 @@ mixin _$TestViewModel on _TestViewModelBase, Store {
   @override
   String toString() {
     return '''
-number: ${number}
+number: ${number},
+isEven: ${isEven}
     ''';
   }
 }
