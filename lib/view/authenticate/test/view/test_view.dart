823acba580ceedf33c11ab345b4f1d62e8d12ebf
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-12 23:50:40 +0300

    observer using example

--- lib/view/authenticate/test/view/test_view.dart
+++ lib/view/authenticate/test/view/test_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_mvvm_template/core/base/state/base_state.dart';
 import 'package:flutter_mvvm_template/core/base/view/base_widget.dart';
 import 'package:flutter_mvvm_template/view/authenticate/test/viewmodel/test_view_model.dart';
+import 'package:mobx/mobx.dart';
 
 class TestsView extends StatefulWidget {
   const TestsView({Key? key}) : super(key: key);
@@ -11,10 +13,28 @@ class TestsView extends StatefulWidget {
 }
 
 class _TestsViewState extends BaseState<TestsView> {
+  TestViewModel? viewModel;
   @override
   Widget build(BuildContext context) {
     return BaseView<TestViewModel>(
         viewModel: TestViewModel(),
+        onModelReady: (model) {
+          viewModel = model;
+        },
         onPageBuilder: (context, value) => Text("data"));
+  }
+
+  //get hızlı set işlemi olmadığı için işlevi sadece ekrana çizmek
+  Widget get scaffoldBody => Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => viewModel?.incrementNumber(),
+        ),
+        body: textNumber,
+      );
+
+  Widget get textNumber {
+    return Observer(
+      builder: (context) => Text(viewModel?.number.toString() ?? ""),
+    );
   }
 }
